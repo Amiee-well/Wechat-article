b52e647851b54e0a0b9b9f66c8e2ebd0dd846d4d
Amiee-well <54148341+Amiee-well@users.noreply.github.com>
2020-07-18 14:17:57 +0800

    定量爬取微信公众号文章

---
+++ cfg.py
@@ -0,0 +1,7 @@
+# 目标公众号标识
+biz = 'MzAwNTA5NTYxOA=='
+# 微信登录后的一些标识参数
+pass_ticket = ''
+appmsg_token = '1055_YAmuAw2QG7dM3aTwSVZVqgtRdct6ilAMTwlz7g~~'
+# 安装的wkhtmltopdf.exe文件路径
+wkhtmltopdf_path = r'E:\wkhtml\wkhtmltopdf\bin\wkhtmltopdf.exe'
